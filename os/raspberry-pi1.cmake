9e0bcf3816e8fdc74eed9d5f064d4a9bc7901747
Ralf Anton Beier <ralf_beier@me.com>
2019-04-15 16:46:42 +0000

    Add pthread by default as compiler/linker option.
    Should fix some problems with cross compilation of ros2.

--- os/raspberry-pi1.cmake
+++ os/raspberry-pi1.cmake
@@ -15,6 +15,8 @@ foreach(_flag -mfpu=vfp)
   polly_add_cache_flag(CMAKE_CXX_FLAGS "${_flag}")
 endforeach()
 
+polly_add_cache_flag(CMAKE_CXX_FLAGS "-pthread")
+
 set(CMAKE_SYSTEM_PROCESSOR "armv6" CACHE INTERNAL "")
 set(RASPBERRY_PI 1 CACHE INTERNAL "")
 set(CMAKE_SYSTEM_NAME "Linux" CACHE INTERNAL "")
