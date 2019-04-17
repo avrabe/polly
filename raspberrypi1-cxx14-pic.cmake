d06f2a5f975b946416e71c013edfae5ae885099a
Ralf Anton Beier <ralf_beier@me.com>
2019-04-17 20:30:29 +0000

    Update raspberry-pi1.cmake

--- raspberrypi1-cxx14-pic.cmake
+++ raspberrypi1-cxx14-pic.cmake
@@ -2,16 +2,16 @@
 # Copyright (c) 2017 Ruslan Baratov
 # All rights reserved.
 
-if(DEFINED POLLY_RASPBERRYPI1_CXX14_PIC_CMAKE_)
+if(DEFINED POLLY_RASPBERRYPI1_CXX14_CMAKE_)
   return()
 else()
-  set(POLLY_RASPBERRYPI1_CXX14_PIC_CMAKE_ 1)
+  set(POLLY_RASPBERRYPI1_CXX14_CMAKE_ 1)
 endif()
 
 include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
 
 polly_init(
-    "RaspberryPi 1 Cross Compile / C++14 / PIC"
+    "RaspberryPi 1 Cross Compile / C++14"
     "Unix Makefiles"
 )
 
@@ -23,5 +23,3 @@ include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx14.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/compiler/gcc-cross-compile-raspberry-pi.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/os/raspberry-pi1.cmake")
 include("${CMAKE_CURRENT_LIST_DIR}/os/raspberry-pi-hardfloat.cmake")
-
-include("${CMAKE_CURRENT_LIST_DIR}/flags/fpic.cmake")
